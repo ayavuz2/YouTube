44c78cd074d494a3f05a0c08b1e05587c79c3b22
Ahmet Yavuz <a.yavuzum@gmail.com>
2020-01-30 06:19:44 +0300

    Test

--- youtube.py
+++ youtube.py
@@ -1,1 +1,17 @@
-print("Hello")
+import requests
+from bs4 import BeautifulSoup
+import  _sqlite3
+
+main_url = "https://watchin.today/charts/channel/country"
+
+r = requests.get(main_url)
+soup = BeautifulSoup(r.content, "html.parser")
+
+var = soup.find_all("a", {"class":"nps-item"})
+var = var[: len(var) - 14]
+
+link_list = []
+for link in var:
+    link_list.append(link.get("href"))
+
+print(link_list)
